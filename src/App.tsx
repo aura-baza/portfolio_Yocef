7e01b57f1a65ffc00baf7043cb18d1d72ba5be07
aura-baza <aurabaza17@gmail.com>
2025-09-28 18:33:56 -0500

    new change 3

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@
 import { Toaster } from '../src/components/ui/sonner';
 import { TooltipProvider } from '../src/components/ui/tooltip';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, HashRouter } from 'react-router-dom';
 import Index from './pages/Index';
 import NotFound from './pages/NotFound';
 
@@ -23,12 +23,12 @@ const App = () => (
   <QueryClientProvider client={queryClient}>
     <TooltipProvider>
       <Toaster />
-      <BrowserRouter>
+      <HashRouter>
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </TooltipProvider>
   </QueryClientProvider>
 );
