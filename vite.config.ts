e5445b792903e9a0647f445a119290bf1d5dc986
aura-baza <aurabaza17@gmail.com>
2025-09-29 17:09:56 -0500

    configuration GitHub  Pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,11 +5,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/',
   plugins: [react(),tailwindcss(),],
    resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
-  },
-  base: '/'
+  }
 })
