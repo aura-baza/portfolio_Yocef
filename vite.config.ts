9b212560ced72e7acb3eb4d4042bd344f714ff8f
aura-baza <aurabaza17@gmail.com>
2025-09-29 17:24:01 -0500

    Configuración base  GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/',
+  base: '/portfolio_yocef/',
   plugins: [react(),tailwindcss(),],
    resolve: {
     alias: {
